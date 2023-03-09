9c3c7fa6f1cace7b43bbfe9ec0c807a06c1769d5
Ryan Gard <ryan.a.gard@msn.com>
2023-03-09 10:17:39 -0800

    chore: update deps
    Updated the deps to the newest versions and now we are using the official
    release of `vitest-environment-prisma` with SQLite support!

--- src/gen/zodPrismaSchemas/index.ts
+++ src/gen/zodPrismaSchemas/index.ts
@@ -1,105 +1,121 @@
-import { z } from "zod";
-import * as PrismaClient from "@prisma/client";
+import { z } from 'zod';
+import { type Prisma } from '@prisma/client';
+
+/////////////////////////////////////////
+// HELPER FUNCTIONS
+/////////////////////////////////////////
+
 
 /////////////////////////////////////////
 // ENUMS
 /////////////////////////////////////////
 
-// PRISMA GENERATED ENUMS
-//------------------------------------------------------
+export const AccountScalarFieldEnumSchema = z.enum(['id','type','provider','providerAccountId','refresh_token','access_token','id_token','token_type','scope','session_state','expires_at','createdAt','updatedAt','userId']);
 
-export const AccountScalarFieldEnumSchema = z.nativeEnum(PrismaClient.Prisma.AccountScalarFieldEnum);
+export const NoteScalarFieldEnumSchema = z.enum(['id','title','markdown','createdAt','updatedAt']);
 
-export const NoteScalarFieldEnumSchema = z.nativeEnum(PrismaClient.Prisma.NoteScalarFieldEnum);
+export const SortOrderSchema = z.enum(['asc','desc']);
 
-export const SortOrderSchema = z.nativeEnum(PrismaClient.Prisma.SortOrder);
+export const TagScalarFieldEnumSchema = z.enum(['id','label','createdAt','updatedAt']);
 
-export const TagScalarFieldEnumSchema = z.nativeEnum(PrismaClient.Prisma.TagScalarFieldEnum);
+export const TagsOnNotesScalarFieldEnumSchema = z.enum(['createdAt','updatedAt','tagId','noteId']);
 
-export const TagsOnNotesScalarFieldEnumSchema = z.nativeEnum(PrismaClient.Prisma.TagsOnNotesScalarFieldEnum);
+export const TransactionIsolationLevelSchema = z.enum(['Serializable']);
 
-export const TransactionIsolationLevelSchema = z.nativeEnum(PrismaClient.Prisma.TransactionIsolationLevel);
+export const UserScalarFieldEnumSchema = z.enum(['id','name','email','emailVerified','image','createdAt','updatedAt']);
 
-export const UserScalarFieldEnumSchema = z.nativeEnum(PrismaClient.Prisma.UserScalarFieldEnum);
-
-export const VerificationTokenScalarFieldEnumSchema = z.nativeEnum(PrismaClient.Prisma.VerificationTokenScalarFieldEnum);
-
-// CUSTOM ENUMS
-//------------------------------------------------------
-
+export const VerificationTokenScalarFieldEnumSchema = z.enum(['identifier','token','expires']);
 /////////////////////////////////////////
 // MODELS
 /////////////////////////////////////////
 
-// TAG
-//------------------------------------------------------
+/////////////////////////////////////////
+// TAG SCHEMA
+/////////////////////////////////////////
 
 export const TagSchema = z.object({
   id: z.string(),
   label: z.string(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+})
 
-// NOTE
-//------------------------------------------------------
+export type Tag = z.infer<typeof TagSchema>
+
+/////////////////////////////////////////
+// NOTE SCHEMA
+/////////////////////////////////////////
 
 export const NoteSchema = z.object({
   id: z.string(),
   title: z.string(),
   markdown: z.string(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+})
 
-// TAGS ON NOTES
-//------------------------------------------------------
+export type Note = z.infer<typeof NoteSchema>
+
+/////////////////////////////////////////
+// TAGS ON NOTES SCHEMA
+/////////////////////////////////////////
 
 export const TagsOnNotesSchema = z.object({
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
   tagId: z.string(),
   noteId: z.string(),
-});
+})
 
-// ACCOUNT
-//------------------------------------------------------
+export type TagsOnNotes = z.infer<typeof TagsOnNotesSchema>
+
+/////////////////////////////////////////
+// ACCOUNT SCHEMA
+/////////////////////////////////////////
 
 export const AccountSchema = z.object({
   id: z.string(),
   type: z.string(),
   provider: z.string(),
   providerAccountId: z.string(),
-  refresh_token: z.string().nullish(),
-  access_token: z.string().nullish(),
-  id_token: z.string().nullish(),
-  token_type: z.string().nullish(),
-  scope: z.string().nullish(),
-  session_state: z.string().nullish(),
-  expires_at: z.number().nullish(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
+  refresh_token: z.string().nullable(),
+  access_token: z.string().nullable(),
+  id_token: z.string().nullable(),
+  token_type: z.string().nullable(),
+  scope: z.string().nullable(),
+  session_state: z.string().nullable(),
+  expires_at: z.number().nullable(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
   userId: z.string(),
-});
+})
 
-// USER
-//------------------------------------------------------
+export type Account = z.infer<typeof AccountSchema>
+
+/////////////////////////////////////////
+// USER SCHEMA
+/////////////////////////////////////////
 
 export const UserSchema = z.object({
   id: z.string(),
-  name: z.string().nullish(),
-  email: z.string().nullish(),
-  emailVerified: z.date().nullish(),
-  image: z.string().nullish(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-});
+  name: z.string().nullable(),
+  email: z.string().nullable(),
+  emailVerified: z.coerce.date().nullable(),
+  image: z.string().nullable(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+})
 
-// VERIFICATION TOKEN
-//------------------------------------------------------
+export type User = z.infer<typeof UserSchema>
+
+/////////////////////////////////////////
+// VERIFICATION TOKEN SCHEMA
+/////////////////////////////////////////
 
 export const VerificationTokenSchema = z.object({
   identifier: z.string(),
   token: z.string(),
-  expires: z.date(),
-});
+  expires: z.coerce.date(),
+})
+
+export type VerificationToken = z.infer<typeof VerificationTokenSchema>
